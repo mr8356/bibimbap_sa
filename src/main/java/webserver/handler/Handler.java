a9e5906fb037258c306eda3ce821729a065f8361
mr8356 <mr8356@naver.com>
2025-04-06 17:37:09 +0900

    init setting

--- src/main/java/webserver/handler/Handler.java
+++
@@ -1,8 +0,0 @@
-package webserver.handler;
-
-import java.io.DataOutputStream;
-import java.io.IOException;
-
-public interface Handler {
-//	void handle(DataOutputStream dos) throws IOException;
-}
