dfac2a81b34cdb1949f7a134d17acce7f1bf252c
mr8356 <mr8356@naver.com>
2025-05-12 16:03:19 +0900

    a ob off

--- src/main/java/com/example/auth/strategy/KakaoLoginStrategy.java
+++ src/main/java/com/example/auth/strategy/KakaoLoginStrategy.java
@@ -4,10 +4,4 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class KakaoLoginStrategy implements LoginStrategy {
-    @Override
-    public void login(String userId) {
-        System.out.println("카카오 로그인 시도: " + userId);
-        System.out.println("카카오 OAuth 인증 진행...");
-        System.out.println("카카오 로그인 성공!");
-    }
 } 
